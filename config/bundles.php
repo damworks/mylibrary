5a7a38501fd93fcfbb7a95c5fa0eb9a7be05bd3d
damworks <larry.chiarello@gmail.com>
2025-11-16 15:25:25 +0100

    Add Pimcore Application Logger bundle to the application configuration

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,11 @@
 <?php
 
+use Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle;
 use Pimcore\Bundle\QuillBundle\PimcoreQuillBundle;
 
 return [
     Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
     PimcoreQuillBundle::class => ['all' => true],
+    PimcoreApplicationLoggerBundle::class => ['all' => true],
+
 ];
