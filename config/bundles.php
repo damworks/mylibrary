e6b533c5fa42c368d0a28c44a9874eb91cbec868
damworks <larry.chiarello@gmail.com>
2025-11-16 15:22:38 +0100

    Add Pimcore Quill bundle to the application configuration

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,8 @@
 <?php
 
+use Pimcore\Bundle\QuillBundle\PimcoreQuillBundle;
+
 return [
     Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+    PimcoreQuillBundle::class => ['all' => true],
 ];
