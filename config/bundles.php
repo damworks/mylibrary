7b27e60a213249a401ce769c558ec3f1baf2289c
damworks <larry.chiarello@gmail.com>
2025-11-16 15:50:56 +0100

    Add Process Manager and Data Hub bundles to application configuration

--- config/bundles.php
+++ config/bundles.php
@@ -1,11 +1,14 @@
 <?php
 
+use Elements\Bundle\ProcessManagerBundle\ElementsProcessManagerBundle;
 use Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle;
+use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
 use Pimcore\Bundle\QuillBundle\PimcoreQuillBundle;
 
 return [
     Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
     PimcoreQuillBundle::class => ['all' => true],
     PimcoreApplicationLoggerBundle::class => ['all' => true],
-
+    PimcoreDataHubBundle::class => ['all' => true],
+    ElementsProcessManagerBundle::class => ['all' => true]
 ];
